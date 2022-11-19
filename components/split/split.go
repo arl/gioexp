efef55815ad393b7f003f2ce1cd998d67aef1ca7
Aurélien Rainone <aurelien.rainone@gmail.com>
2022-11-19 18:57:30 +0100

    components: add split

---
+++ components/split/split.go
@@ -0,0 +1,105 @@
+package split
+
+import (
+	"image"
+
+	"gioui.org/io/pointer"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/unit"
+)
+
+// This comes from gioui.org tutorial, nothing less nothing more.
+
+type (
+	C = layout.Context
+	D = layout.Dimensions
+)
+
+type Split struct {
+	// Ratio keeps the current layout.
+	// 0 is center, -1 completely to the left, 1 completely to the right.
+	Ratio float32
+	// Bar is the width for resizing the layout
+	Bar unit.Dp
+
+	drag   bool
+	dragID pointer.ID
+	dragX  float32
+}
+
+const defaultBarWidth = unit.Dp(10)
+
+func (s *Split) Layout(gtx layout.Context, left, right layout.Widget) layout.Dimensions {
+	bar := gtx.Dp(s.Bar)
+	if bar <= 1 {
+		bar = gtx.Dp(defaultBarWidth)
+	}
+
+	proportion := (s.Ratio + 1) / 2
+	leftsize := int(proportion*float32(gtx.Constraints.Max.X) - float32(bar))
+
+	rightoffset := leftsize + bar
+	rightsize := gtx.Constraints.Max.X - rightoffset
+
+	{ // handle input
+		for _, ev := range gtx.Events(s) {
+			e, ok := ev.(pointer.Event)
+			if !ok {
+				continue
+			}
+
+			switch e.Type {
+			case pointer.Press:
+				if s.drag {
+					break
+				}
+
+				s.dragID = e.PointerID
+				s.dragX = e.Position.X
+
+			case pointer.Drag:
+				if s.dragID != e.PointerID {
+					break
+				}
+
+				deltaX := e.Position.X - s.dragX
+				s.dragX = e.Position.X
+
+				deltaRatio := deltaX * 2 / float32(gtx.Constraints.Max.X)
+				s.Ratio += deltaRatio
+
+			case pointer.Release:
+				fallthrough
+			case pointer.Cancel:
+				s.drag = false
+			}
+		}
+
+		// register for input
+		barRect := image.Rect(leftsize, 0, rightoffset, gtx.Constraints.Max.X)
+		area := clip.Rect(barRect).Push(gtx.Ops)
+		pointer.InputOp{Tag: s,
+			Types: pointer.Press | pointer.Drag | pointer.Release,
+			Grab:  s.drag,
+		}.Add(gtx.Ops)
+		area.Pop()
+	}
+
+	{
+		gtx := gtx
+		gtx.Constraints = layout.Exact(image.Pt(leftsize, gtx.Constraints.Max.Y))
+		left(gtx)
+	}
+
+	{
+		off := op.Offset(image.Pt(rightoffset, 0)).Push(gtx.Ops)
+		gtx := gtx
+		gtx.Constraints = layout.Exact(image.Pt(rightsize, gtx.Constraints.Max.Y))
+		right(gtx)
+		off.Pop()
+	}
+
+	return layout.Dimensions{Size: gtx.Constraints.Max}
+}
